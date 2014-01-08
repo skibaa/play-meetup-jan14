7f7eb8afadaad7404b3f8cb1c2cf3a934c3035dc
Andrew Skiba <skibaa@gmail.com>
2014-01-08 15:51:33 +0200

    Upgrade jquery to 1.10.2, add bootstrap

--- youtube-play-java/app/controllers/Application.java
+++ youtube-play-java/app/controllers/Application.java
@@ -4,11 +4,12 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
 
 }
