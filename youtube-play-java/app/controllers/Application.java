bfb267cec0191f731c7ea728a9955d3614eb2767
Andrew Skiba <skibaa@gmail.com>
2014-01-08 17:47:02 +0200

    player connected to YouTube API

--- youtube-play-java/app/controllers/Application.java
+++ youtube-play-java/app/controllers/Application.java
@@ -5,11 +5,17 @@ import play.mvc.*;
 
 import views.html.*;
 import views.html.index;
+import views.html.player;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render());
+    }
+
+    public static Result player() {
+        int id = (int)(Math.random()*10000);
+        return ok(player.render(id));
     }
 
 }
